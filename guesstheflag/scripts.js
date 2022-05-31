3f1b6a95be4b363685ab6b415f259d7f03732300
yasinworks <egoryas3@gmail.com>
2022-05-31 17:55:23 +0300

    gtf | score and high score

--- guesstheflag/scripts.js
+++ guesstheflag/scripts.js
@@ -1,12 +1,13 @@
-const countryApi = 'https://restcountries.com/v3.1/all';
+const countryApi = 'https://countriesnow.space/api/v0.1/countries/flag/images';
 const startBtn = document.querySelector('#startGame');
 const showNextBtn = document.querySelector('#showNext');
-const scoreSpan = document.querySelector('#score');
 const countryName = document.querySelector('.country-name span');
 const flagsContainer = document.querySelector('.countries');
 let countries = [];
 let questions = [];
 let currentQuestion = 0;
+let score = 0;
+let highScore = localStorage.getItem('highScore');
 
 startBtn.addEventListener('click', () => {
     startGame();
@@ -22,17 +23,19 @@ function createElement(element, className = '') {
     return created;
 }
 
-async function getCountries() {
-    await fetch(countryApi)
-        .then(res => res.json())
-        .then(data => countries = data);
-}
-
 async function startGame() {
     await getCountries();
     await generateQuestion();
+    renderScore();
+    renderHighScore();
 
     renderQuestions();
+}
+
+async function getCountries() {
+    await fetch(countryApi)
+        .then(res => res.json())
+        .then(data => countries = data.data);
 }
 
 function showNextQuestion() {
@@ -43,7 +46,7 @@ function showNextQuestion() {
 
 function generateQuestion() {
     for (let i = 0; i < 10; i++) {
-        const IDs = [Math.round(Math.random() * 250), Math.round(Math.random() * 250), Math.round(Math.random() * 250), Math.round(Math.random() * 250)];
+        const IDs = [Math.round(Math.random() * 219), Math.round(Math.random() * 219), Math.round(Math.random() * 219), Math.round(Math.random() * 219)];
 
         const q = {
             answers: [IDs[0], IDs[1], IDs[2], IDs[3]],
@@ -58,45 +61,67 @@ function generateQuestion() {
 function renderQuestions() {
     const q = questions[currentQuestion];
     const qShuffled = q.answers.sort(() => Math.random() - 0.5);
-    console.log(questions)
+    // console.log(questions)
 
-    countryName.textContent = countries[+q.correctAnswer].name.common;
-
+    countryName.textContent = countries[+q.correctAnswer].name;
     flagsContainer.innerHTML = '';
 
     q.answers.forEach((answer, index) => {
         const flag = createElement('button', 'country-flag');
         const flagImg = createElement('img');
 
-        flagImg.src = countries[+qShuffled[index]].flags.png;
+        flagImg.src = countries[+qShuffled[index]].flag;
         flagImg.dataset.id = answer.toString();
 
         flag.append(flagImg);
         flagsContainer.append(flag);
     });
 
-
     handleAnswer(q.correctAnswer);
+}
+
+function increaseScore() {
+    score++
+    if (highScore < score) updateScoreInLocal() && renderHighScore();
+}
+
+function updateScoreInLocal() {
+    highScore = score;
+    localStorage.setItem('highScore', score);
+}
+
+function renderScore() {
+    const scoreSpan = document.querySelector('#score');
+    scoreSpan.textContent = `Score: ${score}`;
+}
+
+function renderHighScore() {
+    const highScoreSpan = document.querySelector('#highScore');
+    highScoreSpan.textContent = `High score: ${highScore}`;
 }
 
 function handleAnswer(answer) {
     const countries = document.querySelector('.countries');
     const flags = document.querySelectorAll('.country-flag');
 
-    countries.addEventListener('click', (e) => handleConditions(e), {once: true})
+    countries.addEventListener('click', (e) => handleConditions(e), {once: true});
 
     const handleConditions = function(e) {
         if (!e.target.dataset.id) return;
+
         let flag = e.target.parentElement;
+
         if (answer === e.target.dataset.id) {
             flag.classList.add('correct');
-            score++
-            if (currentQuestion < questions.length) showNextBtn.classList.remove('hidden');
+            increaseScore();
+            if (currentQuestion + 1 !== questions.length) showNextBtn.classList.remove('hidden');
         } else {
             flag.classList.add('incorrect');
             flags.forEach(f => { if (answer === f.firstChild.dataset.id) f.classList.add('correct') })
-            if (currentQuestion < questions.length) showNextBtn.classList.remove('hidden');
+            if (currentQuestion + 1 !== questions.length) showNextBtn.classList.remove('hidden');
         }
+
+        renderScore();
     }
 }
 
